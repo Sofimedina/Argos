2595c023ba1e55350cc8bd18c362575c2288aa18
Sofia Medina <sofi.k.medina@gmail.com>
2018-10-08 03:02:38 -0300

    Commit nro°5
    -Se modificaron errores de diseño
    -Se incluyen librerias necesarias en el source

--- src/Retrofit/ApiClient.java
+++ src/Retrofit/ApiClient.java
@@ -6,9 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
         //TODO REVISAR IP
-        //public static String DOMINIO="192.168.1.17";
-        //public static String DOMINIO="192.168.43.87";
-        public static String DOMINIO="sofiserver";
+        public static String DOMINIO="argosserver";
         public static String BASE_URL="http://"+DOMINIO +"/api.argos.com/v1/";
         public static Retrofit retrofit=null;
 
